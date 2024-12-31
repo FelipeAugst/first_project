408ccb50c6b9782f6175401d6c6f4dac805bed04
FelipeAugst <felipeaug92@gmail.com>
2024-12-31 14:18:29 -0300

    start

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use DB;
+use Illuminate\Support\Facades\DB;
+use \App\Models\User
 
 class UserController extends Controller
 {
@@ -23,39 +24,12 @@ class UserController extends Controller
 		$orderby  = $request->input('orderby');
 		$direction = $request->input('direction');
 
-        $users= \App\Models\User::select('id','name','email')
+        $users= User::select('id','name','email')
                               ->orderBy($orderby,$direction)
                              ->paginate($limite);
 
-    // $sql = "select id, name,email from users order by $orderby";
-
-    
-// users = DB::select($sql);
-
-
-
-
-     return response()->json($users, 200);
+     return response()->json($users['data'], 200);
 
     }
 }
 
-$query= DB::connection('mysql')
-			->table('parcelas')
-			->select(
-					 'parcelas.id',
-                    'parcelas.contrato_id',
-                    'parcelas.galaxPayId',
-                    'parcelas.valor',
-                    'parcelas.nparcela',
-                    'parcelas.data_vencimento',
-                    'parcelas.data_pagamento',
-                    'parcelas.data_baixa',
-                    'parcelas.valor_pago',
-                    'parcelas.boletobankNumber',
-                    'parcelas.statusDescription'
-                                                                )
-            ->where('parcelas.contrato_id','=',$contrato_id);
-        $query->orderBy($orderby,$direction);
-		
-        $parcelas = $query->paginate($limite);
